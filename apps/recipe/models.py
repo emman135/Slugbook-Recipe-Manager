1b22a4e1c2fcff168ddac3a7dcac90e709d521a1
Nathan Dong <nathandong.dev@gmail.com>
2025-06-11 00:06:45 -0700

    connect to api and add meals

--- apps/recipe/models.py
+++ apps/recipe/models.py
@@ -1,10 +1,12 @@
 from pydal.validators import *
 from .common import Field, db, auth
+import requests
+import re
 
 db.define_table(
     "ingredients",
     Field("name", type="string", requires=IS_NOT_EMPTY()),
-    Field("unit", type="integer", requires=IS_INT_IN_RANGE(0,1000)),
+    Field("unit", type="string", requires=IS_NOT_EMPTY()),
     Field("calories_per_unit", type="integer", requires=IS_INT_IN_RANGE(0,1000)),
     Field("description", type="string", requires=IS_NOT_EMPTY()),
 )
@@ -27,8 +29,33 @@ db.define_table(
     Field("quantity_per_serving", type="integer", requires=IS_INT_IN_RANGE(0,1000)),
 )
 
-db.commit()
+# Connects to TheMealDB API and fills the database with recipes
+def populate_db():
+    base_api_url = "www.themealdb.com/api/json/v1/1/search.php?f="
+    all_meals = []
 
-# for table in db.tables:
-#     db[table].drop()
-# db.commit()
+    print("Accessing TheMealDB API")
+    for letter in "abcdefghijklmnopqrstuvwxyz":
+        try:
+            response = requests.get(base_api_url + letter)
+            response.raise_for_status()
+            data = response.json()
+            if (data.get(meals)):
+                all_meals.extend(data["meals"])
+        except requests.exceptions.RequestException as e:
+            print(f"Could not fetch recipes for letter '{letter}': {e}")
+
+    for meal in meals:
+        recipe_id = db.recipes.insert(
+            name=meal.get("strMeal"),
+            type=meal.get("strCategory"),
+            description=f'{meal.get("strArea")} cuisine',
+            image=meal.get("strMealThumb"),
+            instruction_steps=meal.get("strInstructions"),
+            servings=1,
+            author=None,  # No author, imported from API
+        )
+
+
+
+db.commit()
